61ba16b4557de229bd8759c774e08eb8f35411b5
benzics <benjaminnicholas29@gmail.com>
2023-03-02 13:59:13 +0100

    added sidebar links and pages

---
+++ client/src/pages/agents.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Agents = () => {
+  return (
+    <div>agents</div>
+  )
+}
+
+export default Agents
---
+++ client/src/pages/index.ts
@@ -0,0 +1,21 @@
+import AgentProfile from "./agent-profile";
+import Agents from "./agents";
+import Home from "./home";
+import CreateProperty from "./create-property";
+import EditProperty from "./edit-property";
+import MyProfile from "./my-profile";
+import PropertyDetails from "./property-details";
+import { Login } from "./login";
+import AllProperties from "./all-properties";
+
+export {
+    AgentProfile,
+    Agents,
+    Home,
+    CreateProperty,
+    EditProperty,
+    MyProfile,
+    PropertyDetails,
+    Login,
+    AllProperties
+}
